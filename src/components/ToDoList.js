7ce9160753af3a78fde4a946a070bbc5fef65079
Valentin-Marko <69463262+Valentin-Marko@users.noreply.github.com>
2020-11-11 18:39:06 +0200

    adding project

---
+++ src/components/ToDoList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Todo from "./Todo";
+
+const ToDoList = ({ todos, filteredTodos, setTodos }) => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            key={todo.id}
+            text={todo.text}
+            todos={todos}
+            todo={todo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDoList;
